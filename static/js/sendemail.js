a3faa9026cca5134aebf74099bec2644866be745
Ubuntu <ubuntu@ip-172-31-42-20.eu-west-1.compute.internal>
2020-03-06 02:19:14 +0000

    Changed background colour on Recipe Page

--- static/js/sendemail.js
+++ static/js/sendemail.js
@@ -1,4 +1,4 @@
-var myform = $("form#contactForm");
+/*var myform = $("form#contactForm");
 myform.submit(function(event){
     event.preventDefault();
 
@@ -19,39 +19,23 @@ myform.submit(function(event){
     });
   return false;
 });
-
-
-
-
-
-
-
-/*    alert("hi");
-    emailjs.sendForm('gmail', 'template_EUYyvjOQ', this);
-}*/
-
-//console.log('Hello');
-
-/*emailjs.send("gmail", "miscsoletrader", {
-    "from_name":"H Mansuri",
-    "from_email":"hidayat.mansuri@gmail.com",
-    "from_amazon":"132-54654-654564",
-    "website_contact":"Hi,"
-    
-})var templateParams = {
-    name: 'James',
-    notes: 'Check this out!'
-};
-
-var templateParams = {
-    name: 'James',
-    notes: 'Check this out!'
-};
- 
-emailjs.send('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', templateParams)
-    .then(function(response) {
-       console.log('SUCCESS!', response.status, response.text);
-    }, function(error) {
-       console.log('FAILED...', error);
-    });
 */
+var myform = $("form#contactForm");
+myform.submit(function(event){
+	event.preventDefault();
+
+  // Change to your service ID, or keep using the default service
+  var service_id = "gmail";
+  var template_id = "miscsoletrader";
+
+  myform.find(".btn").text("Sending...");
+  emailjs.sendForm(service_id,template_id,myform[0])
+  	.then(function(){ 
+    	alert("Sent!");
+       myform.find(".btn").text("Send");
+    }, function(err) {
+       alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+       myform.find("btn").text("Send");
+    });
+  return false;
+});
